0361be4109c78df36e78b31f44570558d5831b40
Prasanna Poonacha <Ppoonacha@mast.com>
2019-03-06 13:23:24 +0530

    added configs and updated scripts

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,16 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
 import requireContext from 'require-context.macro';
 import 'storybook-chromatic/storybook-addon';
 import '../src/index.css';
 
 const req = requireContext('../src/components', true, /.stories.js$/);
+
+addParameters({
+  options: {
+    name: 'Components'
+    // more configuration here
+  },
+});
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
---
+++ storybook-static/precache-manifest.9f958f4c1802a8ac7663b368d6e043b1.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = [
+  {
+    "url": "vendors~main.22c0375c98c1098e7a79.bundle.js"
+  },
+  {
+    "url": "runtime~main.22c0375c98c1098e7a79.bundle.js"
+  },
+  {
+    "revision": "e7199ac337b7641eafec",
+    "url": "main.css"
+  },
+  {
+    "url": "main.22c0375c98c1098e7a79.bundle.js"
+  },
+  {
+    "revision": "c9e49da48dbcbdc43897448a486f1311",
+    "url": "iframe.html"
+  }
+];
