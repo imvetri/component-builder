132e5f4ee04ef965483d78aa85530efc1b97366b
Prasanna Poonacha <Ppoonacha@mast.com>
2019-03-06 19:00:11 +0530

    added viewport addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,15 +1,39 @@
 import { configure, addParameters } from '@storybook/react';
 import requireContext from 'require-context.macro';
 import 'storybook-chromatic/storybook-addon';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import '../src/index.css';
 
 const req = requireContext('../src/components', true, /.stories.js$/);
+
+const newViewports = {
+  kindleFire2: {
+    name: 'Kindle Fire 2',
+    styles: {
+      width: '600px',
+      height: '963px',
+    },
+  },
+  kindleFireHD: {
+    name: 'Kindle Fire HD',
+    styles: {
+      width: '533px',
+      height: '801px',
+    },
+  },
+};
 
 addParameters({
   options: {
     name: 'Components'
     // more configuration here
   },
+  viewport: {
+    viewports: {
+      ...INITIAL_VIEWPORTS,
+      ...newViewports,
+    }
+  }
 });
 
 function loadStories() {
